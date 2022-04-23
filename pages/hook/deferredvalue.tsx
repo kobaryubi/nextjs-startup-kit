fd0fb45efb0868bc39605394a7de5fde5ed60a8d
koba <koba@growal.com>
2022-04-23 02:01:11 +0000

    d

--- pages/hook/deferredvalue.tsx
+++ pages/hook/deferredvalue.tsx
@@ -1,32 +1,36 @@
-import { FC, memo, useState, useDeferredValue } from 'react'
+import { useState, useDeferredValue, useTransition, Suspense } from 'react'
 import { NextPage } from 'next'
 
 const HookDeferredValue: NextPage = () => {
   const [text, setText] = useState("");
-  const deferredText = useDeferredValue(text);
+  const [dataId, setDataId] = useState(0);
+  const [isPending, startTransition] = useTransition()
 
   return (
     <>
-      <input value={text} onChange={event => setText(event.currentTarget.value)} />
-      <HeavyTenThousand text={deferredText} />
+      <p>
+        <input
+          value={text}
+          onChange={event => {
+            const newText = event.currentTarget.value;
+            setText(newText)
+            startTransition(() => {
+              setDataId(newText.length)
+            })
+          }}
+        />
+      </p>
+      <Suspense fallback={<p>Loading...</p>}>
+        <p>DATA ID: {dataId}</p>
+        <LoadedContents dataId={dataId} />
+      </Suspense>
     </>
   )
 }
 
 export default HookDeferredValue;
 
-const HeavyTenThousand: FC<{ text: string }> = memo(({ text }) => (
-  <p>
-    {Array.from({length: 100}).map((_, i) => (
-      <HeavyHundred key={i} text={text} />
-    ))}
-  </p>
-))
-
-const HeavyHundred: FC<{ text: string }> = ({text}) => (
-  <>
-    {Array.from({ length: 100 }).map((_, i) => (
-      <span key={i}>{text}</span>
-    ))}
-  </>
-)
+const LoadedContents: React.FC<{ dataId: number }> = ({ dataId }) => {
+  const data = "TEST"
+  return <p>{data}</p>;
+};
