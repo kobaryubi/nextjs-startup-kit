419a823a1e173cde9a473d62b25f27e03229c8f9
koba <koba@growal.com>
2022-04-28 13:33:18 +0000

    story

---
+++ components/Task.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+
+export type Props = {
+  id: string;
+  title: string;
+  state: string;
+  onArchiveTask: (id: string) => void;
+  onPinTask: (id: string) => void;
+}
+
+const Task: FC<Props> = ({ id, title, state, onArchiveTask, onPinTask }) => {
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">      
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(id)}
+          id={`archiveTask-${id}`}
+          aria-label={`archiveTask-${id}`}
+        />
+      </label>
+      <div className="title">
+        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+      </div>
+      <div className="actions" onClick={event => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} id={`pinTask-${id}`} aria-label={`pinTask-${id}`} />
+          </a>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Task
