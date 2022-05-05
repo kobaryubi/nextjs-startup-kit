da6c4ea8818d84a3f263d4df679757d34f12080b
koba <koba@growal.com>
2022-05-05 12:25:46 +0000

    l

---
+++ components/TaskList.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import Task, { Props as TaskProps } from 'components/Task';
+
+export type Props = {
+  loading: boolean,
+  tasks: TaskProps[]
+}
+
+const TaskList: FC<Props> = ({loading, tasks }) => {
+
+  if (loading) {
+    return <div className="list-items">loading</div>
+  }
+
+  if (tasks.length === 0) {
+    return <div className="list-items">empty</div>
+  }
+
+  return (
+    <div className="list-items">
+      {tasks.map(task => (
+        <Task key={task.id} {...task} />
+      ))}
+    </div>
+  )
+}
+
+export default TaskList;
