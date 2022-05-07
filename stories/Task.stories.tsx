ff8a9f32620ae5149086f9ba2390a6589fce7a0c
koba <koba@growal.com>
2022-05-07 11:40:32 +0000

    s

--- stories/Task.stories.tsx
+++ stories/Task.stories.tsx
@@ -1,18 +1,21 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Task from '../components/Task'
+import { action } from '@storybook/addon-actions';
 
 export default {
   component: Task,
   title: 'Task',
 } as ComponentMeta<typeof Task>
 
-const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
+const Template: ComponentStory<typeof Task> = args => <Task {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   id: '1',
   title: 'Test Task',
   state: 'TASK_INBOX',
+  onPinTask: action('onPinTask'),
+  onArchiveTask: action('onArchiveTask'),
 }
 
 export const Pinned = Template.bind({});
