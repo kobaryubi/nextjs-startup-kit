d14d39bb964ada6bbb44ce2bfd9ec2e14a679258
koba <koba@growal.com>
2022-06-14 23:00:32 +0000

    feat: provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { Provider } from 'react-redux'
+import { store } from '@/app/store'
 
 const queryClient = new QueryClient()
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </QueryClientProvider>
   )
 }
