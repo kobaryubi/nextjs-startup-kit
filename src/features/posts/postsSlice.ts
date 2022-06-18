e577c13b2cb79264f6704ced67bc9e0a55c441f5
koba <koba@growal.com>
2022-06-18 01:47:27 +0000

    feat: slice

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -14,7 +14,12 @@ const initialState: Post[] = [
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {}
+  reducers: {
+    postAdded: (state, action) => {
+      state.push(action.payload)
+    }
+  }
 })
 
+export const { postAdded } = postsSlice.actions
 export default postsSlice.reducer
