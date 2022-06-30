4af5d1cc35d62122718725c1ee6e267fa1d95f5e
koba <koba@growal.com>
2022-06-30 22:58:21 +0000

    feat: checkout

---
+++ pages/stripe/checkout.tsx
@@ -0,0 +1,94 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form';
+
+const ProductDisplay = () => {
+  const {
+    handleSubmit
+  } = useForm()
+
+  const onSubmit = async () => {
+    const url = "/api/stripe/create-checkout-session"
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ lookup_key: "test_subscription_product" })
+    })
+    console.log(await response.json())
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <button type="submit">Checkout</button>
+    </form>
+  )
+}
+
+const SuccessDisplay = ({ sessionId }: {sessionId: string}) => {
+  const { handleSubmit } = useForm()
+
+  const onSubmit = async () => {
+    const url = "/api/stripe/create-portal-session"
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ session_id: sessionId })
+    })
+    console.log(await response.json())
+  }
+
+  return (
+    <div>
+      <p>successed</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <button type="submit">portal</button>
+      </form>
+    </div>
+  )
+}
+
+const Message = ({ message }: { message: string }) => {
+  return (
+    <p>{message}</p>
+  )
+}
+
+const StripeCheckout = () => {
+  const [message, setMessage] = useState('')
+  const [sessionId, setSettionId] = useState('')
+  const [hasSuccessed, setHasSuccessed] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!router.isReady) {
+      return;
+    }
+
+    const { success, canceled, session_id } = router.query
+    if (success) {
+      setHasSuccessed(true)
+      setSettionId(session_id as string)
+    }
+
+    if (canceled) {
+      setHasSuccessed(false)
+      setMessage("canceled")
+    }
+  }, [router.isReady, router.query])
+
+  if (!hasSuccessed && !message) {
+    return <ProductDisplay />
+  }
+
+  if (hasSuccessed && sessionId) {
+    return <SuccessDisplay sessionId={sessionId} />
+  }
+  
+  return <Message message={message} />
+}
+
+export default StripeCheckout;
