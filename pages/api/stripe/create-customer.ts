ae4fca68890d7ea6e39fb081ba40b650e9968619
koba <koba@growal.com>
2022-07-03 13:54:33 +0000

    feat: customer

---
+++ pages/api/stripe/create-customer.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { stripe } from '@/libs/stripe'
+import { serialize } from 'cookie'
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  await stripe.prices.list()
+  const { email } = req.body;
+  const { data } = await stripe.customers.list({ email });
+  const customer = data.length >= 1 ? data[0] : await stripe.customers.create({ email })
+
+  res.setHeader("Set-Cookie", serialize("customer", customer.id, { maxAge: 900000, httpOnly: true }))
+  res.status(200).json({ customer })
+};
+
+export default handler;
