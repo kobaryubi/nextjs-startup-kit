b7c7dabad37a5d9d3c10e7aa07a1a1a6aa103402
koba <koba@growal.com>
2022-07-07 13:12:23 +0000

    feat: use stripe

--- src/hook/useStripe.ts
+++ src/hook/useStripe.ts
@@ -1,4 +1,4 @@
 import { useContext } from "react";
 import { StripeContext } from "src/context/StripeContext";
 
-export const useStripeSubscription = () => useContext(StripeContext)
+export const useStripe = () => useContext(StripeContext)
